8d5e901925bc84d6559e959586cea66f63352528
gacharsadok <73200566+gacharsadok@users.noreply.github.com>
2022-11-14 03:31:21 +0100

    Update StockRestController.java

--- src/main/java/tn/esprit/rh/achat/controllers/StockRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/StockRestController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @Api(tags = "Gestion des stocks")
 @RequestMapping("/stock")
-@CrossOrigin("*")
+@CrossOrigin("origins = "http://192.168.1.8:8089")
 public class StockRestController {
 
 	@Autowired
